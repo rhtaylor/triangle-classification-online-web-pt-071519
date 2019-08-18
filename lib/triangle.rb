2550be6cb610bc04ede8a8ad5d148644479d3b4f
Randy Taylor <randyisbusy@gmail.com>
2019-08-18 17:51:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,26 +1,26 @@
-class Triangle 
+class Triangle
   def initialize(side1, side2, side3)
-    @side1 = side1 
-    @side2 = side2 
-    @side3 = side3 
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
 
-def kind 
-  if side1 && side2 && side3 <= 0 
-    begin 
-      raise CustomEr 
-    rescue CustomEr => error 
-      puts error.message 
+def kind
+  if side1 && side2 && side3 <= 0
+    begin
+      raise CustomEr
+    rescue CustomEr => error
+      puts error.message
     end
-else 
+else
   if side1 && side2 && side3 == 0
-    :equilateral 
+    :equilateral
   end
-end   
+end
   class CustomEr < StandardError
-    def message 
+    def message
       "FUCK BRO KEEP GOING, Go!, GO!, GO!"
     end
   end
-end 
+end
   
